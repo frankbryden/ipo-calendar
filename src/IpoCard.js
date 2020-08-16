132fd206bb362949329a4d1821bd03c281264627
frankbryden <frank.bryden@gmail.com>
2020-08-16 18:26:09 +0200

    added colors

--- src/IpoCard.js
+++ src/IpoCard.js
@@ -8,22 +8,24 @@ class IpoCard extends React.Component {
         super();
     }
 
-    
 
-    render () {
+
+    render() {
         return (
             <div>
-                <Card 
-                className="card"
-                title={this.props.ipo.name} 
-                style={{ width:400, height: 700, margin: 20}} 
-                cover={<img alt="Example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"></img>}
-                hoverable>
+                <Card
+                    className="card"
+                    title={this.props.ipo.name}
+                    style={{ width: 400, height: 700, margin: 20 }}
+                    cover={<img alt="Example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"></img>}
+                    hoverable>
                     <p className="marketCap"><strong>{this.props.ipo.marketCap}</strong></p>
                     <p className="description">{this.props.ipo.description}</p>
                     <p className="status">{this.props.ipo.status}</p>
-                    <div className="tags">{this.props.ipo.tags.map(tag => <div className="tag">{tag.name}</div>)}</div>
-                </Card>      
+                    <div className="tags">
+                        {this.props.ipo.tags.map(tag => <div style={{ backgroundColor: tag.color }} className="tag">{tag.name}</div>)}
+                    </div>
+                </Card>
             </div>
         )
     }
