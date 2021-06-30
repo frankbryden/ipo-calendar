79c7d767fcb9510f0c7bea8139b15bb72ebf87a5
maximilianvb <62935939+maximilianvb@users.noreply.github.com>
2021-06-30 12:16:58 +0200

    init

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
+app.use(cors());
 const tagprocessing = require('./tag-processing.js');
 const dataUtils = require('./data-utils.js');
 const requestIp = require('request-ip');
